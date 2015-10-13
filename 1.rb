025ffcc84f1135bd19fd1efeee17fbb4fd652a61
Pavel Bezpalov <pavel.bezpalov@cyber-wizard.com>
2015-10-13 09:32:09 +0300

    Get rid of unnecessary variable

--- 1.rb
+++ 1.rb
@@ -1,7 +1,5 @@
 def sum(val = 0)
-  sum = 0
-  val.to_s.gsub(/[^0-9]/, '').each_char { |c| sum += c.to_i }
-  sum
+  val.to_s.split('').map(&:to_i).reduce(:+)
 end
 
 puts "sum(23324) = #{sum(233_24)}"
